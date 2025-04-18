46c03bfbb8e0b4c4d22dc41aff29e1a6f03ea088
TristanRenard <renard.tristan@proton.me>
2025-04-18 14:42:59 +0200

    chore: add Tailwind CSS configuration and PostCSS setup
    - Created postcss.config.mjs to configure PostCSS with Tailwind CSS plugin.
    - Added tailwind.config.js for Tailwind CSS setup, specifying content paths and using NativeWind preset.
    - Updated tsconfig.json to include nativewind-env.d.ts for TypeScript support.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <SafeAreaView
+      className="h-full bg-red-500 flex flex-col items-center justify-start"
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    </SafeAreaView >
+  )
 }
