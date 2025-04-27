f36d8dcfc22444376f9715ac47a67132d19a3f76
TristanRenard <renard.tristan@proton.me>
2025-04-27 17:44:30 +0200

    feat: integrate QueryClientProvider and add chatbot and cart tabs to layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,56 @@
 import { Ionicons } from "@expo/vector-icons"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Tabs } from "expo-router"
 
+const queryClient = new QueryClient()
+
 const TabLayout = () => (
-  <Tabs screenOptions={{ headerShown: false }}>
-    <Tabs.Screen
-      name="index"
-      options={{
-        title: 'Accueil',
-        tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
-      }}
-    />
-    <Tabs.Screen
-      name="products"
-      options={{
-        title: 'Produits',
-        tabBarIcon: ({ color }) => <Ionicons name="grid" size={24} color={color} />,
-      }}
-    />
-  </Tabs>
+  <QueryClientProvider client={queryClient}>
+    <Tabs screenOptions={{ headerShown: false }} >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Accueil',
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="products"
+        options={{
+          title: 'Produits',
+          tabBarIcon: ({ color }) => <Ionicons name="grid" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="chatbot"
+        options={{
+          title: 'Chatbot',
+          tabBarIcon: ({ color }) => <Ionicons name="chatbubble" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: 'Panier',
+          tabBarIcon: ({ color }) => <Ionicons name="cart" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="auth/account"
+        options={{
+          title: 'Compte',
+          tabBarIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="auth/login"
+
+        options={{
+          href: null,
+        }}
+      />
+    </Tabs>
+  </QueryClientProvider>
 )
 
 export default TabLayout
